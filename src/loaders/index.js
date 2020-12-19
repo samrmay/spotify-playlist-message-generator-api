aee76cd5062791114f7e946f996d05d2e8764317
samrmay <samrmay@sas.upenn.edu>
2020-12-18 20:23:10 -0500

    added fetch and env support

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,6 +1,8 @@
 import loadExpress from "./express.js";
 
 export default async (app) => {
+  require("dotenv").config();
+
   console.log("Loading express");
   loadExpress(app);
   console.log("Express loaded");
